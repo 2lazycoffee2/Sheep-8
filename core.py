5364e0dad2fd35f554aea9a1928f4b0a49f9031a
2lazycoffee2 <francisfrance255@gmail.com>
2025-03-31 17:16:52 +0200

    implémentation du chargement du fichier, alloc de mémoire

---
+++ core.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+#import pygame
+
+memory = 4096
+VX= [0]*16
+stack = [16]
+
+# chargement de la rom, chargeé -> stocké dans une var, convertir, envoyé en mémoire boucle : 
+
+rom = open("rom/IBM.ch8","r")
+
+    
+            

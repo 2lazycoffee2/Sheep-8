3819f72fad1df28d38c7356b4bcc4fec80afd6d2
2lazycoffee2 <francisfrance255@gmail.com>
2025-04-01 22:56:31 +0200

    concaténation de deux octets fait, récupération de l'opcode fait.

--- core.py
+++ core.py
@@ -27,18 +27,34 @@ Pointer = [0] * 16          # un pointeur pour les sprintes e mémoire (0x000 ->
 
 with open("rom/IBM.ch8", "rb") as file:
     data_rom = file.read()
-    #print("voici votre rom :\n{}".format(data_rom))
-    print ("ici la taille de la rom :\n {}".format(len(data_rom)))
+    #print("here is your rom :\n{}".format(data_rom))
+    #print ("length rom :\n {}".format(len(data_rom)))
 
 
 #affichage et sauvegarde de la rom : 
 for i in range(len(data_rom)):
     memory[PC + i] = data_rom[i]
-    print(memory[PC + i])              #lecture de la rom faites en décimale, reconversion en hexa ??
+    #print(memory[PC + i])            
 
+
+
+
+
+
+shiftleftbyte = (memory[PC] << 8) | memory[PC + 1]      # Comme les instructions sont encodés sur 2 octets, et qu'un octet est 2 caractères hexadécimaux, on décale le premier octet(deux premiers caractère hexa) que l'on décale vers la gauche d'un octet et, que l'on racolle à l'octet suivant (soit le pc + 1 ième) 
+
+
+
+print("catched instruct in decimal : {}\ncatched instruc in hexa : {} \n".format(shiftleftbyte, hex(shiftleftbyte)))
+
+print("\n ALL DATA IN DECIMAL :\n {}". format(memory))
 
 
 #while (true)
+#if opcode == 00E0
+    
+
+
 """
 ici se trouvera une boucle infini pour le fetch, decode, execute, la lecture a été faite précédement.
 Etape suivante : décoder l'hexa reçue avec l'histoire des nibbles (demi-octets)

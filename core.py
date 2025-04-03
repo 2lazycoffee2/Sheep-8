bd4d39abdf67d614c9f8d959ba3d4686bb420731
2lazycoffee2 <francisfrance255@gmail.com>
2025-04-03 19:24:33 +0200

    section des caractères en nugbells et  sauvegarde des grignotages

--- core.py
+++ core.py
@@ -11,8 +11,6 @@ SP = []                     # Pointeur de pile
 
 PC = 0x200                  #Program counter, pour passer d'instruction en instruction.
 
-
-
 Delay_timmer = [0]*8 
 Delay_sound = [0] * 8
 
@@ -20,9 +18,7 @@ Pointer = [0] * 16          # un pointeur pour les sprintes e mémoire (0x000 ->
 
 
 
-
 # chargement de la rom, chargeé -> stocké dans une var, convertir, envoyé en mémoire boucle : 
-
 
 
 with open("rom/IBM.ch8", "rb") as file:
@@ -38,23 +34,36 @@ for i in range(len(data_rom)):
 
 
 
-
-
-
 shiftleftbyte = (memory[PC] << 8) | memory[PC + 1]      # Comme les instructions sont encodés sur 2 octets, et qu'un octet est 2 caractères hexadécimaux, on décale le premier octet(deux premiers caractère hexa) que l'on décale vers la gauche d'un octet et, que l'on racolle à l'octet suivant (soit le pc + 1 ième) 
-
 
 
 print("catched instruct in decimal : {}\ncatched instruc in hexa : {} \n".format(shiftleftbyte, hex(shiftleftbyte)))
 
-print("\n ALL DATA IN DECIMAL :\n {}". format(memory))
+""""nubble def :"""     #définition de chaque demi octet et, de leurs gignotage. Permettra d'associer par exemple le X au registre VX
 
 
-#while (true)
-#if opcode == 00E0
-    
+# Ici, pour les demi octets (un caractère hexa) je le récupère avec un ET logique. C'est un masquage puis un décalage vers la droite. Explication
+Nug1 = (shiftleftbyte & 0xF000) >> 12       # l'exemple ici soit 0xABCD. On fait l'opération suivante : 0b 1010 1011 1100 1101 que l'on "mutliplie" par 0b 1111 0000 0000 0000, on récupère 0xA000 puis on décale de 12 bits vers la droite.
+Nug2 = (shiftleftbyte & 0x0F00) >> 8
+Nug3 = (shiftleftbyte & 0x00F0) >> 4
+Nug4 = (shiftleftbyte & 0x000F)
+
+# Un exemple pour bien comprendre : supposons que j'attrape l'instruction 0xABCD (Qui n'existe pas bien sure). Alors on a : 
+X = Nug2   #A
+Y = Nug3   #B   
+N = Nug4   #C
+NN = Nug3 << 4 | Nug4 #CD
+NNN = (Nug2 << 4 | Nug3) << 4 | Nug4 # BCD 
 
 
+print("concatenation : \n{}, {}, {}, {}, {}".format(X, Y, N, NN, NNN))
+print("HEXA concatenation : \n{}, {}, {}, {}, {}".format(hex(X), hex(Y), hex(N), hex(NN), hex(NNN)))
+
+
+
+
+
+PC += 2
 """
 ici se trouvera une boucle infini pour le fetch, decode, execute, la lecture a été faite précédement.
 Etape suivante : décoder l'hexa reçue avec l'histoire des nibbles (demi-octets)

4cb997e0ab2173e2dc18563fdb79ba26641c61cf
2lazycoffee2 <francisfrance255@gmail.com>
2025-04-19 15:09:51 +0200

    classe cpu ajouté avec référence sur l'affichage (display), manque d'opcode, manque l'affichage à implémenté en POO, faire les tests dans un fichier main.py

--- core.py
+++ core.py
@@ -5,8 +5,10 @@ class CPU:
     Classe définissant le coeur de la console : 
     """
 
-    def __init__(self, rom_file, font_file):
-
+    def __init__(self, rom_file, font_file, display, Win_buffer):
+        """
+        Constructeur de la classe CPU
+        """
         self.rom = rom_file
         self.font = font_file
 
@@ -15,9 +17,17 @@ class CPU:
         self.VX = [0]*16
         self.stack = [0]*16
         self.Index = 0
+        # serviront à l'écran, le cpu en dépendera pour l'instant je n'ai pas trouver meilleure alternative
+        self.display = display
+        self.Win_buffer = Win_buffer
+
+
 
     def load_font(self):
-
+        """
+        Fonction de chargement de la 
+        police de caractère en mémoire.
+        """
         with open(self.font, "rb") as font:
             font_data = font.read()
         font.close()
@@ -27,14 +37,24 @@ class CPU:
 
     
     def load_rom(self) : 
+        """
+        Fonction de chargement de la rom
+        en mémoire.
+        """
         with open(self.rom, "rb") as rom:
             rom_data = rom.read()
 
         for cells in range(len(rom_data)):
             self.memory[self.PC + cells] = rom_data[cells]
 
-    def decode(self):
 
+    def decode(self):
+        """
+        Fonction décodeur, elle lit ce qui 
+        est récupéré en mémoire et le décode.
+        Un découpage des caractères hexadécimaux,
+        expliqué dans les commentaires est réalisé.
+        """
         Instruction = (self.memory[self.PC] << 8) | self.memory[self.PC + 1]
 
         # Ici, pour les demi octets (un caractère hexa) je le récupère avec un ET logique.>
@@ -54,9 +74,71 @@ class CPU:
 
         print("Position : ({}, {})\nDécoupage : 1-{}, 2-{}, 3-{}".format(X,Y , N, NN, NNN))
         # Pour plus de détail, lire la doc mais, ce découpage est nécessaire au bon fonctionnement de la console.
+       
+        return Instruction, Nug1, Nug2, Nug3, Nug4, X, Y, N, NN, NNN
+    
+    
 
-        def pipeline(self):
-            print("encours de développement...")
+
+    def pipeline(self):
+        """
+        fonction fetch/decode/execute, 
+        elle émule le pipeline de la 
+        console.
+        """
+        Instruction, Nug1, Nug2, Nug3, Nug4, X, Y, N, NN, NNN = self.decode()
+        
+        if Instruction == 0x00e0 : 
+            self.PC += 2
+
+        elif Instruction == 0x00ee:
+            #initier l'écran comme une matrice 64*32px contenant des 0
+            self.PC = self.stack.pop()
+            #print("depop la pile, retour à l'adresse contenue dans la stack")
+            self.PC +=2
+
+        elif Nug1 == 0x1:
+            self.PC = NNN
+            #print("Mise de PC à  |{}| | PROG COUNTER : {}".format(NNN, PC))
+
+        elif Nug1 == 0x2:
+            #appelle du sous programme. 
+            self.stack.append(self.PC)                         # le append c'est comme un push en assembleur. (cf archi ordi)
+            self.PC +=2
+
+        elif Nug1 == 0x6:
+            self.VX[X] = NN
+            #print("Mise de VX à |{}| | REGISTRE : ".format(NN, VX))
+            self.PC +=2
+
+        elif Nug1 == 0x7:
+            self.VX[X] += NN
+            #print("Somme de |{}| et VX | REGISTRE : {}".format(NN, VX))
+            self.PC +=2
+
+        elif Nug1 == 0xa:
+            self.Index = NNN
+            self.PC +=2
+
+        elif Nug1 == 0xd:
+            self.VX[0xF]=0
+            for line in range(N):
+                Spryte_Byte = self.memory[self.Index +line]
+
+                for colone in range(8):
+                    px = (Spryte_Byte >> (7 - colone)) & 1     
+                    X_pos = (self.VX[self.X] + colone) % 64
+                    Y_pos = (self.VY[self.Y] + line) % 32
+
+                    if px == 1:    
+                        self.Win_buffer[Y_pos][X_pos] ^=1
+                    
+                    if self.Win_buffer[Y_pos][X_pos] == 1:
+                            self.VX[0xF] = 1
+                            self.display(Y_pos, X_pos)
+            self.PC += 2
+                      
+        print("encours de développement...")
 
 
 coreprocess = CPU("rom/IBM.ch8", "font/chip48font.txt")

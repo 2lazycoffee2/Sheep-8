94e5218fe559fd948ab69d99adad1fc33b97b0db
TortipOOF <victormolis@outlook.com>
2025-04-22 16:24:31 +0200

    Création de la classe Display

--- display.py
+++ display.py
@@ -1,6 +1,25 @@
-import pygame
+#coding:utf-8
 
-pygame.init()
-size = width, height = 1280, 720
+import pygame as ga
 
-screen = pygame.display.set_mode(size)
+class Display:
+
+    """
+    Classe permettant de gérer l'affichage de l'écran 64x32 du CHIP-8
+    """
+
+    def __init__(): 
+        """
+        Constructeur de la classe Display
+        """
+        # Initialisation de Pygame
+        ga.init()
+
+        # Création de la fenêtre
+        self.window = ga.display.set_mode((640, 320))
+
+        # Remplissage de la fenêtre avec du noir (les pixels étant noirs par défaut)
+        Window.fill((0, 0, 0))
+
+        # Création d'un tableau de 32x64 pour le buffer d'affichage
+        Win_Buffer = [[0 for _ in range(64)] for _ in range(32)]

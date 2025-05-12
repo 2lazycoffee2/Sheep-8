d20806e5bbd22e696867b2d55c10ba6e888da543
TortipOOF <victormolis@outlook.com>
2025-05-12 22:52:16 +0200

    Ajout échelle (scale)

--- display.py
+++ display.py
@@ -12,32 +12,33 @@ class Display:
         """
         Constructeur de la classe Display
         """
+        
         # Initialisation de Pygame
         ga.init()
 
+        # Dimensions de l'écran CHIP-8
+        self.width = 64
+        self.height = 32
+        self.scale = 10
+
         # Création de la fenêtre
-        self.window = ga.display.set_mode((640, 320))
+        self.window = ga.display.set_mode((self.width * self.scale, self.height * self.scale))
 
-        # Remplissage de la fenêtre avec du noir (les pixels étant noirs par défaut)
-        self.window.fill((0, 0, 0))
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
 
-        # Création d'un tableau de 32x64 pour le buffer d'affichage
-        self.Win_Buffer = [[0 for _ in range(64)] for _ in range(32)]
+        self.Win_Buffer = [[0 for _ in range(self.width)] for _ in range(self.height)]
+        self.window.fill(self.black)
 
     def draw_px(self, X_pos, Y_pos, px):
         """
         Dessine un pixel à la position (x, y) avec une logique XOR.
         """
-        if 0 <= X_pos < 64 and 0 <= Y_pos < 32:
+        if 0 <= X_pos < self.width and 0 <= Y_pos < self.height:
             if px == 1:
-                if self.Win_Buffer[Y_pos][X_pos] == 1:
-                    self.Win_Buffer[Y_pos][X_pos] = 0
-                else:
-                    self.Win_Buffer[Y_pos][X_pos] = 1
+                self.Win_Buffer[Y_pos][X_pos] ^= 1
 
-            pixel_width = self.window.get_width() // 64
-            pixel_height = self.window.get_height() // 32
-            rect = ga.Rect(X_pos * pixel_width, Y_pos * pixel_height, pixel_width, pixel_height)
-            color = (255, 255, 255) if self.Win_Buffer[Y_pos][X_pos] == 1 else (0, 0, 0)
+            rect = ga.Rect(X_pos * self.scale, Y_pos * self.scale, self.scale, self.scale)
+            color = self.white if self.Win_Buffer[Y_pos][X_pos] == 1 else self.black
             ga.draw.rect(self.window, color, rect)
             ga.display.update()

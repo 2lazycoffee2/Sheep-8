153a26267280289bb1cef0be677714ef6dd49dac
2lazycoffee2 <francisfrance255@gmail.com>
2025-05-28 18:23:29 +0200

    Ajout du pipeline dans le fichier principale : CHIP-8.py. ATTENTION, bien repenser à retirer les parties de l'affichage mis en commentaire lors du merge.

--- CHIP-8.py
+++ CHIP-8.py
@@ -1,13 +1,45 @@
 #coding:utf-8
 
-#import Display as display
+#import display as disp
 
-import core as Co
+import core as co
+import input as controller
+import pygame as ga
 
-coreprocess = Co.CPU("rom/IBM.ch8")#, "font/chip48font.txt")
+
+coreprocess.decode()
+coreprocess.pipeline()
+
+
+input0 = controller.Input()
+file = input("path of your file (don't forgot the .ch8 at the end ! ) : ")
+file = str(file)
+coreprocess = co.CPU("rom/IBM.ch8", "font/chip48font.txt")
 
 coreprocess.load_font()
 coreprocess.load_rom()
 
-coreprocess.decode()
-coreprocess.pipeline()
+#monitor = disp.Display()
+#beep = ga.mixer.Sound('sound/beep.wav')
+beep_channel = ga.mixer.Channel(0)
+
+# LOOP :
+
+while True:
+    events = ga.event.get()
+
+
+    input0.update(events)
+
+    coreprocess.decode()                                                       #decode fonctionnelle
+    coreprocess.pipeline()                                                     #pipeline fonctionnelle
+    if coreprocess.delay_timer > 0:
+        coreprocess.delay_timer -=1
+    if coreprocess.sound_timer > 0:
+        if not beep_channel.get_busy():
+            beep_channel.play(beep)
+        coreprocess.sound_timer -= 1
+    else:
+        beep_channel.stop()
+    #monitor.Draw_pixel(coreprocess.Win_buffer)
+    ga.time.Clock().tick(-30)
